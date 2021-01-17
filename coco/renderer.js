81262ce3b8f0a7121321d78bae1711aaef0af996
Jongyeon Yoon <0417yjy@naver.com>
2021-01-18 00:59:11 +0900

    Separate window control script from renderer.js

--- coco/renderer.js
+++ coco/renderer.js
@@ -35,62 +35,6 @@ function check_dbconnect(param) {
     }
 }
 
-// bootstrap.js를 사용하는 데 필요
-window.$ = window.jquery = require("jquery");
-window.popper = require("popper.js");
-const bootstrap = require("bootstrap");
-
-const remote = require('electron').remote;
-
-const win = remote.getCurrentWindow(); /* Note this is different to the
-html global `window` variable */
-
-// When document has loaded, initialise
-document.onreadystatechange = (event) => {
-    if (document.readyState == "complete") {
-        handleWindowControls();
-    }
-};
-
-window.onbeforeunload = (event) => {
-    /* If window is reloaded, remove win event listeners
-    (DOM element listeners get auto garbage collected but not
-    Electron win listeners as the win is not dereferenced unless closed) */
-    win.removeAllListeners();
-}
-
-function handleWindowControls() {
-    // Make minimise/maximise/restore/close buttons work when they are clicked
-    document.getElementById('min-button').addEventListener("click", event => {
-        win.minimize();
-    });
-
-    document.getElementById('max-button').addEventListener("click", event => {
-        win.maximize();
-    });
-
-    document.getElementById('restore-button').addEventListener("click", event => {
-        win.unmaximize();
-    });
-
-    document.getElementById('close-button').addEventListener("click", event => {
-        win.close();
-    });
-
-    // Toggle maximise/restore buttons when maximisation/unmaximisation occurs
-    toggleMaxRestoreButtons();
-    win.on('maximize', toggleMaxRestoreButtons);
-    win.on('unmaximize', toggleMaxRestoreButtons);
-
-    function toggleMaxRestoreButtons() {
-        if (win.isMaximized()) {
-            document.body.classList.add('maximized');
-        } else {
-            document.body.classList.remove('maximized');
-        }
-    }
-}
-
 // --------------------------------------------- 모달 스크립트 ----------------------------------------------
 const modal_type = Object.freeze({ YESNO: 0, OK: 1, TEXT_INPUT: 2 });
 function show_modal(mode, modal_header, modal_body) {
