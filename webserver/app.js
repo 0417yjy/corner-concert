3d3e8cd37fab6af20d2d55be2639f95763639b8c
Jongyeon Yoon <0417yjy@naver.com>
2021-01-25 03:28:13 +0900

    Using dotenv

--- webserver/app.js
+++ webserver/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 var db = require('./dbconn')();
+require('dotenv').config(); 
 
 // init db connection
 db.init();
